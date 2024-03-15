3f9e2c1a7e7bf425c6da89615ca7ac1758cc6970
joshuasdasd <joshua.medina@student.ateneo.edu>
2024-03-15 14:57:41 +0800

    added migrations fodler to ledger

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import Recipe, RecipeIngredient, Ingredient
+from django.contrib.auth.models import User
+
+from .models import Profile
 # Register your models here.
 
 class RecipeIngredientInline(admin.TabularInline):
@@ -13,5 +17,16 @@ class RecipeAdmin(admin.ModelAdmin):
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [
+        ProfileInline,
+    ]
+
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
