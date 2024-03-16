8135b3fd157013aab425fef840618d23faa1777e
joshuasdasd <joshua.medina@student.ateneo.edu>
2024-03-17 00:20:39 +0800

    removed urls still trying to figure out why password reset n others arent being overrided

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 from .models import Recipe
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView, PasswordResetView
+
 
 
 
@@ -16,9 +16,7 @@ class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'ledger/recipe_details.html'
 
-class CustomLoginView(LoginView):
-    template_name = 'registration/login.html'
 
-class CustomPasswordResetView(PasswordResetView):
-    template_name = 'registration/password_reset_form.html'
+
+
     
